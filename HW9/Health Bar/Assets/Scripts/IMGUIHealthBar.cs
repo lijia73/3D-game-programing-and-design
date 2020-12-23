3d0df21f55aff32a999fc512e231de00a74c5b70
lijia73 <gaillee666@outlook.com>
2020-12-23 20:10:18 +0800

    complete

---
+++ HW9/Health Bar/Assets/Scripts/IMGUIHealthBar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IMGUIHealthBar : MonoBehaviour
+{
+    public float health = 0f;
+    private Rect healthBar;
+    public Rect healthUp;
+    public Rect healthDown;
+    public Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBar = new Rect(50, 50, 200, 30);
+        healthUp = new Rect(75, 80, 40, 30);
+        healthDown = new Rect(175, 80, 40, 30);
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(healthUp, "+"))
+        {
+            health = health + 0.1f > 1f ? 1f : health + 0.1f;
+        }
+        if (GUI.Button(healthDown, "-"))
+        {
+            health = health - 0.1f < 0 ? 0 : health - 0.1f;
+        }
+        slider.value = health;
+        GUI.HorizontalScrollbar(healthBar, 0f, health, 0f, 1f);
+    }
+}
---
+++ HW9/Health Bar/Assets/Scripts/Move.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translationX = Input.GetAxis("Horizontal");
+        float translationZ = Input.GetAxis("Vertical");
+        MovePlayer(translationX, translationZ);
+    }
+    
+    public void MovePlayer(float translationX, float translationZ){
+        translationX *= Time.deltaTime;
+        translationZ *= Time.deltaTime;
+        this.transform.LookAt(new Vector3(this.transform.position.x + translationX, this.transform.position.y, this.transform.position.z + translationZ));
+        if (translationX == 0)
+            this.transform.Translate(0, 0, Mathf.Abs(translationZ) * 5);
+        else if (translationZ == 0)
+            this.transform.Translate(0, 0, Mathf.Abs(translationX) * 5);
+        else
+            this.transform.Translate(0, 0, (Mathf.Abs(translationZ) + Mathf.Abs(translationX)) * 2.5f);
+    }
+}
